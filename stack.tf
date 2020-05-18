a082cd2067524b823becd182d82ccae5851b05a4
Agniv Roy Choudhury <agnivroychoudhury@Agnivs-MBP.fios-router.home>
2020-05-18 14:58:57 -0400

    initial

--- stack.tf
+++ stack.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "mytfsg" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-0323c3dd2da7fb37d"
-   instance_type = "t2.micro"
+   ami = var.instanceId
+   instance_type = var.instanceType
    vpc_security_group_ids = [aws_security_group.mytfsg.id]
 }
